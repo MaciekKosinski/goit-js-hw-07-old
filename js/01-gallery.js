209d3ed0adc999d8fba09aeee2e61bad4a14cb7d
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-05-23 19:55:39 +0200

    finish homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const photoGallery = document.querySelector(".gallery");
+photoGallery.innerHTML = createGalleryMarkup(galleryItems);
+photoGallery.addEventListener("click", onIMGModalOpen);
+
+let modalInstance;
+
+function onIMGModalOpen(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  modalInstance = basicLightbox.create(
+    `<img width="1400" height="900"
+    src="${event.target.dataset.source}">`,
+    {
+      onShow: (modalInstance) => {
+        addListener();
+      },
+      onClose: (modalInstance) => {
+        removeListener();
+      },
+    }
+  );
+  modalInstance.show();
+}
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function addListener() {
+  window.addEventListener("keydown", onEscClick);
+}
+
+function removeListener() {
+  window.removeEventListener("keydown", onEscClick);
+}
+
+function onEscClick(e) {
+  if (e.code === "Escape") {
+    modalInstance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const reference = {
+    galleryContainer: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+reference.galleryContainer.innerHTML = galleryMarkup;
+
+const galleryModalWindow = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(item => createGalleryItem(item)).join('');
+}
+
+function createGalleryItem({ preview, original, description }) {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          loading="lazy"
+        />
+      </a>`;
+}
